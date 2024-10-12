6c826fc3a042eb797152579483e1453ac8a6b751
Chervin Tanilon <chervin.tanilon@kodakollectiv.com>
2024-10-12 16:18:14 +0800

    Fix: CI config env set

--- tests/summary.test.js
+++ tests/summary.test.js
@@ -7,7 +7,7 @@ describe('Summary API', () => {
   let summaryId
 
   beforeAll(async () => {
-    await mongoose.connect("mongodb+srv://tanilonchervin:wyoOypHqoJfrnClQ@cluster0.r5kog.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { dbName: 'website_scraper' })
+    await mongoose.connect(process.env.MONGO_URI, { dbName: 'website_scraper' })
 
     const summary = await Summary.create({
       url: 'http://example.com',
@@ -15,24 +15,24 @@ describe('Summary API', () => {
       summary: 'This is a test summary.'
     })
     summaryId = summary._id
-  }, 10000)
+  })
 
   afterAll(async () => {
     await Summary.findByIdAndDelete(summaryId)
     await mongoose.disconnect()
-  }, 10000)
+  })
 
   describe('GET /api/summary/:id', () => {
     it('should return the summary by ID', async () => {
       const res = await request(app).get(`/api/summary/${summaryId}`)
       expect(res.statusCode).toEqual(200)
       expect(res.body).toHaveProperty('summary', 'This is a test summary.')
-    }, 10000)
+    })
 
     it('should return 404 if summary not found', async () => {
       const res = await request(app).get('/api/summary/123') // Use an invalid ID
       expect(res.statusCode).toEqual(404)
       expect(res.body).toHaveProperty('message', 'Summary not found')
-    }, 10000)
+    })
   })
 })
